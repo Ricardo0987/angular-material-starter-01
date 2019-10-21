bbc75dc9e0845f01d642cc94ee7ebb65fa2caefd
Ricardo0987 <ricardorlo@hotmail.es>
2019-10-21 11:29:25 -0500

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,27 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 /**
  * @title Basic Inputs
  */
 @Component({
-  selector: 'app-component',
-  styleUrls: ['app.component.css'],
-  templateUrl: 'app.component.html',
+  selector: "app-component",
+  styleUrls: ["app.component.css"],
+  templateUrl: "app.component.html"
 })
-export class AppComponent {}
-
+export class AppComponent {
+  test1() {
+    console.log("prueba");
+  }
+  test2() {
+    console.log("prueba");
+  }
+  test3() {
+    console.log("prueba");
+  }
+  test1() {
+    console.log("prueba");
+  }
+}
 
 /**  Copyright 2018 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
