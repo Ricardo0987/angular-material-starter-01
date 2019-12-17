2bac8686be8e271bf8f7441521c98a26e1cbea4a
Ricardo0987 <ricardorlo@hotmail.es>
2019-12-17 14:25:15 -0500

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from "@angular/core";
  * @title Basic Inputs
  */
 @Component({
+
+
+  
   selector: "app-component",
   styleUrls: ["app.component.css"],
   templateUrl: "app.component.html"
